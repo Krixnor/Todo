7bb6a36b9b19c5f165d3c467bb3bff9dfa8e696b
Christabel <udodorchristabel@gmail.com>
2024-08-03 02:21:59 +0200

    solving allowed host issue

--- userAuth/settings.py
+++ userAuth/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['todo-d0o1.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
