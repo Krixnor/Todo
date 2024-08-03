4ac94a2ec2919f4ac546d7cc57e956fd36e01e5d
Christabel <udodorchristabel@gmail.com>
2024-08-03 02:43:32 +0200

    resolving resource not found

--- userAuth/urls.py
+++ userAuth/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf.urls.static import static
+from django.conf import settings
 from users import views
 
 urlpatterns = [
@@ -30,4 +32,10 @@ urlpatterns = [
     path('account/index/', views.index, name='index'), 
     
     path('apis/', include('api_rest.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns= urlpatterns+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
